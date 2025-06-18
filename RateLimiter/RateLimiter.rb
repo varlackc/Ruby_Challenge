8dba313d7c897d526b2e6c5550440858b55e133b
Maxwell Varlack <varlackc@mail.gvsu.edu>
2025-06-18 00:10:07 -0600

    add modularity and encapsulation features to the RateLimiter application

--- RateLimiter/RateLimiter.rb
+++ RateLimiter/RateLimiter.rb
@@ -7,17 +7,36 @@ class RateLimiter
     @user_requests = Hash.new { |hash, key| hash[key] = []}
   end
 
-  def allow_request?(timestamp, user_id)
-    # returns true if request is allowed, false otherwise
+  # private method used for modularity and encapsulation
+  private
+  def update_sliding_window(timestamp, user_id)
     # clear the sliding window
-    @user_requests[user_id].reject! { |t| t <= timestamp - @time_window}
+    @user_requests[user_id].reject! { |t| t <= timestamp - @time_window }
+  end
+
+  # private method used for modularity and encapsulation
+  private
+  def verify_number_of_elements(timestamp, user_id)
     # verify if the number of elements is less than the max_request
     if @user_requests[user_id].size < @max_requests
       # add new value to the list associated with the user_id
       @user_requests[user_id].append(timestamp)
+      # if so return true
       return true
+    # otherwise return false
+    else
+      return false
     end
-    # return false if the conditions for the request are not met
-    return false
+  end
+
+  # public method
+  public
+  def allow_request?(timestamp, user_id)
+    # returns true if request is allowed, false otherwise
+    # clear the sliding window
+    update_sliding_window(timestamp, user_id)
+    # verify number of elements
+    result = verify_number_of_elements(timestamp, user_id)
+    return result
   end
 end
--- RateLimiter/RateLimiter_spec.rb
+++ RateLimiter/RateLimiter_spec.rb
@@ -5,30 +5,54 @@ describe RateLimiter do
   let(:time_window) { 30 } # 30 seconds time window
   let(:max_requests) { 3 } # maximum 3 requests allowed
   let(:rate_limiter) { RateLimiter.new(time_window, max_requests) }
-
-  context 'when requests are within the limit' do
-    it 'allows requests within the time window' do
-      expect(rate_limiter.allow_request?(1700000010, 1)).to be true
-      expect(rate_limiter.allow_request?(1700000011, 2)).to be true
-      expect(rate_limiter.allow_request?(1700000020, 1)).to be true
+  
+  # helper function to simplify the test calls
+  def expect_call(time_stamp, user_id, result)
+    expect(rate_limiter.allow_request?(time_stamp, user_id)).to be result
+  end
+  
+  # helper function to simplify the data processing step  
+  def test_data_call(context_data)
+    # loop to test the values in the data structure
+    context_data.each do |entry|
+      # get the values for the data structure from each entry
+      timestamp, user_id, result = entry[0], entry[1], entry[2]
+      # execute the test call
+      expect_call(timestamp, user_id, result)
     end
   end
 
-  context 'when requests exceed the limit' do
-    it 'blocks requests exceeding the limit within the time window' do
-      expect(rate_limiter.allow_request?(1700000010, 1)).to be true
-      expect(rate_limiter.allow_request?(1700000011, 1)).to be true
-      expect(rate_limiter.allow_request?(1700000020, 1)).to be true
-      expect(rate_limiter.allow_request?(1700000035, 1)).to be false # Exceeds limit
+  action = 'when requests are within the limit'
+  response = 'allows requests within the time window'
+  data = [[1700000010, 1, true],[1700000011, 2, true],[1700000020, 1, true]]
+  
+  context action do
+    it response do
+      test_data_call(data)
+    end
+  end
+  
+  action = 'when requests exceed the limit'
+  response = 'blocks requests exceeding the limit within the time window'
+  # Exceeds limit
+  data = [[1700000010, 1, true],[1700000011, 1, true],[1700000020, 1, true], 
+  [1700000035, 1, false]]
+  
+  context action do
+    it response do
+      test_data_call(data)
     end
   end
 
-  context 'when requests are outside the time window' do
-    it 'allows requests after the time window has passed' do
-      expect(rate_limiter.allow_request?(1700000010, 1)).to be true
-      expect(rate_limiter.allow_request?(1700000011, 1)).to be true
-      expect(rate_limiter.allow_request?(1700000020, 1)).to be true
-      expect(rate_limiter.allow_request?(1700000041, 1)).to be true # Outside time window
+  action = 'when requests are outside the time window'
+  response = 'allows requests after the time window has passed'
+  # Outside time window
+  data = [[1700000010, 1, true],[1700000011, 1, true],[1700000020, 1, true],
+  [1700000041, 1, true]]
+  
+  context action do
+    it response do
+      test_data_call(data)
     end
   end
 
